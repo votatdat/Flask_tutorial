788f8ab720caca213672cde19ec2dfea7edded9e
Vo Tat Dat <tatdat13@gmail.com>
2021-03-26 10:18:07 +0700

    Finished chap03 forms

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
-from flask import render_template
+from flask import render_template, redirect, flash, url_for
 
 from app import app
+from app.forms import LoginForm
 
 
 @app.route('/')
@@ -18,3 +19,13 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template(url_for('index'), title='Sign In', form=form)
